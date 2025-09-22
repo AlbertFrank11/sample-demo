fa0f2dc97c2c3ae25305bba4734a37e7f47cb68a
root <root@ip-172-31-21-126.ec2.internal>
2025-09-22 10:02:01 +0000

    infra2

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,17 @@
+# main.tf
+
+# --- Provider Block ---
 provider "aws" {
-  region = "us-east-1"
+  region                  = "us-east-1"
+  shared_credentials_file = "/root/.aws/credentials"
+  profile                 = "default"
 }
 
+# --- Resource Block ---
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
+
   tags = {
     Name = "app-server"
   }
